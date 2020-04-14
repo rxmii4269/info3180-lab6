867945dfa63421534994de18f1a20132e9a12730
romaine murray <romainemurray6@gmail.com>
2020-04-14 02:02:49 -0500

    Completed lab6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,6 @@
 /* Add your Application JavaScript */
 Vue.component('app-header', {
-    template: `
+  template: `
         <header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
               <a class="navbar-brand" href="#">VueJS App</a>
@@ -11,41 +11,110 @@ Vue.component('app-header', {
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                   <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
+                    <router-link to="/" class="nav-link">Home</router-link>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
+                    <router-link to="/news" class="nav-link">News</router-link>
                   </li>
                 </ul>
               </div>
             </nav>
         </header>    
     `,
-    data: function() {
-      return {};
-    }
+  data: () => {
+    return {};
+  }
 });
 
 Vue.component('app-footer', {
-    template: `
+  template: `
         <footer>
             <div class="container">
                 <p>Copyright &copy {{ year }} Flask Inc.</p>
             </div>
         </footer>
     `,
-    data: function() {
-        return {
-            year: (new Date).getFullYear()
-        }
+  data: () => {
+    return {
+      year: (new Date).getFullYear()
     }
+  }
 })
 
 
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
+const NewsList = Vue.component('news-list', {
+  template: `
+      <div class="news">
+      <h2>News</h2>
+      <div class="form-inline d-flex justify-content-center">
+        <div class="form-group mx-sm-3 mb-2">
+          <label class="sr-only" for="search">Search</label>
+          <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+          <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+          </div> 
+        </div>
+        <ul class="news__list">
+          <li v-for="article in articles" class="news__item">{{article.title}} <img :src="article.urlToImage"/> <p>{{article.description}}</p> </li>
+        </ul>
+      </div>
+  `,
+  created: function() {
+    let self = this;
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      console.log(data);
+      self.articles = data.articles;
+    });
+  },
+  data: ()=> {
+    return {
+      articles: [],
+      searchTerm: ''
     }
+  },
+  methods:{
+    searchNews: function() {
+      let self = this;
+
+      fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en&apiKey=')
+      .then((response)=>{
+        return response.json();
+      })
+      .then((data)=>{
+        console.log(data);
+        self.articles = data.articles;
+      });
+    }
+  }
 });
 
+
+const Home = Vue.component('home',{
+  template: `
+    <div class="home">
+      <img src="/static/images/logo.png" alt="VueJS Logo">
+      <h1>{{ welcome }}</h1>
+    </div> 
+  `,
+  data: ()=>{
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  }
+});
+
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path:'/',component: Home},
+    {path:'/news',component: NewsList}
+  ]
+})
+
+let app = new Vue({
+  el: '#app',
+  router
+});
